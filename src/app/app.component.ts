b67d79a955b78d3de1ac7dc12563a490897886e6
md-redwan-hossain <redwan.ping@gmail.com>
2024-06-19 12:50:38 +0600

    added tabler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,74 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from "@angular/forms";
+import { filter } from "rxjs";
+import { InputGroupModule } from "primeng/inputgroup";
+import { InputGroupAddonModule } from "primeng/inputgroupaddon";
+import { InputTextModule } from "primeng/inputtext";
+import { CheckboxModule } from "primeng/checkbox";
+import { ButtonModule } from "primeng/button";
+import { TooltipModule } from "primeng/tooltip";
+
+interface TodoForm {
+  title: FormControl<string>;
+  isCompleted: FormControl<boolean>;
+}
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    ReactiveFormsModule,
+    ButtonModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    CheckboxModule,
+    TooltipModule
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss"
 })
-export class AppComponent {
-  title = 'todo-app';
+export class AppComponent implements OnInit {
+  constructor(private readonly formBuilder: NonNullableFormBuilder) {}
+
+  todoContainerForm = this.formBuilder.group({
+    todos: this.formBuilder.array<FormGroup<TodoForm>>([])
+  });
+
+  get todos() {
+    return this.todoContainerForm.controls.todos;
+  }
+
+  idResolver(elem: string, index: number) {
+    return elem.concat(index.toString());
+  }
+
+  addTodo(data?: TodoForm) {
+    const itemForm = this.formBuilder.group({
+      title: [data?.title || "", Validators.required],
+      isCompleted: [data?.isCompleted || false, Validators.required]
+    });
+    this.todos.push(itemForm);
+  }
+
+  deleteTodo(todoIndex: number) {
+    this.todos.removeAt(todoIndex);
+  }
+
+  ngOnInit() {
+    const formData = localStorage.getItem("TODO_DATA");
+    if (formData) {
+      JSON.parse(formData).forEach((item: TodoForm) => this.addTodo(item));
+    }
+
+    this.todoContainerForm.valueChanges
+      .pipe(filter(() => this.todoContainerForm.valid))
+      .subscribe((val) => localStorage.setItem("TODO_DATA", JSON.stringify(val.todos)));
+  }
 }
